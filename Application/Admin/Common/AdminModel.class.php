9cf479b3a29c5aac4e6d47ef5af48d3152621904
licutung <784855684@qq.com>
2015-12-19 23:17:01 +0800

    home

---
+++ Application/Admin/Common/AdminModel.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lin
+ * Date: 2015/12/19
+ * Time: 20:50
+ */
+namespace Application\Admin\Common\Model;
+use System\Core\Model;
+
+class AdminModel extends Model {
+
+}
