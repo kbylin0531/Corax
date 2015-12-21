34d4cbd9c8153d21eda72cb16d8de1f7d783fdd5
lichtung <784855684@qq.com>
2015-12-21 16:42:30 +0800

    com

---
+++ Application/Admin/System/Controller/MenuController.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/12/21
+ * Time: 16:03
+ */
+namespace Application\Admin\System\Controller;
+use Application\Admin\Common\Controller\AdminController;
+
+/**
+ * Class MenuController 系统后台菜单设置
+ * @package Application\Admin\System\Controller
+ */
+class MenuController extends AdminController {
+
+    public function index(){
+        echo __METHOD__;
+    }
+
+}
---
+++ Application/Admin/System/Model/MenuModel.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/12/21
+ * Time: 16:05
+ */
+namespace Application\Admin\System\Model;
+use Application\Admin\Common\Model\AdminModel;
+
+/**
+ * Class MenuModel 菜单设置
+ * @package Application\Admin\System\Model
+ */
+class MenuModel extends AdminModel {
+
+
+
+}
